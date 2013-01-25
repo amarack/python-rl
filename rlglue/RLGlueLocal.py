54d90d0682253bd99d9990fb8d79a718e7c10757
Will Dabney <amarack@gmail.com>
2013-01-24 21:45:37 -0500

    This mimics the behavior of LocalGlue for the other codecs, which is a funcationality that the python codec seems to be lacking. In some quick tests it is many times faster than running the python with sockets (duh). From the user perspective functions nearly identically to the network based RLGlue interface, only difference is that the agent and environment are passed into the constructor.

---
+++ rlglue/RLGlueLocal.py
@@ -0,0 +1,121 @@
+# 
+# Copyright (C) 2007, Mark Lee
+# 
+#http://rl-glue-ext.googlecode.com/
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+#  $Revision: 473 $
+#  $Date: 2009-01-29 20:50:12 -0700 (Thu, 29 Jan 2009) $
+#  $Author: brian@tannerpages.com $
+#  $HeadURL: http://rl-glue-ext.googlecode.com/svn/trunk/projects/codecs/Python/src/rlglue/RLGlue.py $
+
+import sys
+import os
+from rlglue.agent.Agent import Agent
+from rlglue.environment.Environment import Environment
+
+from rlglue.types import Action
+from rlglue.types import Observation
+
+from rlglue.types import Observation_action
+from rlglue.types import Reward_observation_action_terminal
+from rlglue.types import Reward_observation_terminal
+
+
+class LocalGlue:
+	def __init__(self,theEnvironment,theAgent):
+		self.env = theEnvironment
+		self.agent = theAgent
+		self.prevact = None
+		self.reward_return = 0.0
+		self.step_count = 0
+		self.episode_count = 0
+
+	def RL_init(self):
+		taskSpecResponse = self.env.env_init()
+		self.agent.agent_init(taskSpecResponse)
+		self.prevact = None
+		self.reward_return = 0.0
+		self.step_count = 0
+		self.episode_count = 0
+		return taskSpecResponse
+
+	def RL_start(self):
+		self.reward_return = 0.0
+		self.step_count = 1
+		self.episode_count += 1
+		self.prevact = None
+		obs = self.env.env_start()
+		action = self.agent.agent_start(obs)
+		obsact = Observation_action()
+		obsact.o = obs
+		obsact.a = action
+		self.prevact = action
+		return obsact
+
+	def RL_step(self):
+		if self.prevact is None:
+			self.RL_start()
+		self.step_count += 1
+		rot = self.env.env_step(self.prevact)
+		roat = Reward_observation_action_terminal()
+		roat.terminal = rot.terminal
+
+		if rot.terminal == 1:
+			self.agent.agent_end(rot.r)
+			roat.a = self.prevact
+			self.prevact = None
+		else:
+			self.prevact = self.agent.agent_step(rot.r, rot.o)
+			roat.a = self.prevact
+
+		self.reward_return += rot.r
+		roat.r = rot.r
+		roat.o = rot.o
+		return roat
+
+	def RL_cleanup(self):
+		self.env.env_cleanup()
+		self.agent.agent_cleanup()
+
+	def RL_agent_message(self, message):
+		if message == None:
+			message=""
+		return self.agent.agent_message(message)
+
+	def RL_env_message(self, message):
+		if message == None:
+			message=""
+		return self.env.env_message(message)
+
+	def RL_return(self):
+		return self.reward_return
+
+	def RL_num_steps(self):
+		return self.step_count
+
+	def RL_num_episodes(self):
+		return self.episode_count
+
+	def RL_episode(self, num_steps):
+		exitStatus = 0
+		self.RL_start()
+		while exitStatus != 1:
+			if (num_steps > 0) and self.step_count >= num_steps:
+				break
+			roat = self.RL_step()
+			exitStatus = roat.terminal
+		return exitStatus
+
